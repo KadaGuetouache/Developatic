85ea3111cec7053c9585222f1efa2a5b940c342e
KadaGuetouache <wail@disroot.org>
2023-06-22 23:36:38 +0100

    create seeder to generate 1000 user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,28 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+         $faker = Faker::create();
+
+        $users = [];
+
+        for ($i = 0; $i < 1000; $i++) {
+            $users[] = [
+                'name' => $faker->name(),
+                'email' => $faker->email(),
+                'password' => $faker->password(),
+                'date_of_birth' => $faker->dateTimeBetween('-75 years', '-15 years')->format('Y-m-d'),
+                'user_type' => 'standard'
+            ];
+        }
+
+        $users[] = [
+            'name' => 'Administrator',
+            'email' => 'admin@something.com',
+            'password' => 'password',
+            'date_of_birth' => $faker->dateTimeBetween('-75 years', '-15 years')->format('Y-m-d'),
+            'user_type' => 'admin'
+        ];
+
+        DB::table('users')->insert($users);
     }
 }
